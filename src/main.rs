064832b9b0e626ce4e89334dc017c54b9ad34794
TilakMaddy <tilak.madichetti@gmail.com>
2025-07-03 12:06:51 +0530

    chore

--- src/main.rs
+++ src/main.rs
@@ -29,22 +29,21 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    // Initialize system state
     let shared_state = FullSystemState::new_in_rwlock(cli_args.node_id);
 
-    // Routes for server
+    // routes
     let app = Router::new()
         .route("/broadcast", post(serve::broadcast_message))
         .route("/receive", post(serve::receive_message))
         .with_state(shared_state);
 
-    // Listen on port 3000 + node ID
+    // listen
     let address = format!("0.0.0.0:{}", 3000 + cli_args.node_id);
     let listener = tokio::net::TcpListener::bind(address.clone())
         .await
         .unwrap();
 
-    // Serve
+    // serve
     tracing::trace!("listening on {}", address);
     axum::serve(listener, app).await.unwrap();
 }
